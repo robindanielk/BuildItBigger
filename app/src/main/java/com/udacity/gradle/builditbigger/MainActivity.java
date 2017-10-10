f89e14874c5c62829c75ad680c5fd401644f3480
robin <robindanielk@gmail.com>
2017-10-10 15:16:27 +0530

    Added paid and free flavors

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,18 +1,15 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.example.JokeProviderJava;
-import com.example.myandroidlibrary.JokeTellerAndroid;
+import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
+import com.udacity.gradle.builditbigger.MainActivityFragment;
 
 
 public class MainActivity extends AppCompatActivity {
